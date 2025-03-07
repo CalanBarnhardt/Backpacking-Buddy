f5c3650a1c662f56b281f2f421d006a451fe5b5c
Jacey Jonson <jjonson@mines.edu>
2025-03-07 11:14:38 -0700

    Added TripItinerarySpec and added routes

---
+++ app/src/main/java/pangolin/backpackingbuddy/ui/navigation/specs/TripItinerarySpec.kt
@@ -0,0 +1,66 @@
+package pangolin.backpackingbuddy.ui.navigation.specs
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.core.content.PackageManagerCompat.LOG_TAG
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHostController
+import pangolin.backpackingbuddy.R
+import pangolin.backpackingbuddy.ui.exisitngTripItinerary.ExisitngTripItinerary
+import pangolin.backpackingbuddy.viewmodel.BackpackingBuddyViewModel
+import java.util.UUID
+
+data object TripItinerarySpec : IScreenSpec {
+    override val arguments: List<NamedNavArgument> = emptyList()
+
+    private const val ROUTE_BASE = "trip_itinerary"
+    private const val ARG_UUID_NAME = "uuid"
+
+    private fun buildFullRoute(argVal: String): String {
+        var fullRoute = ROUTE_BASE
+        if(argVal == ARG_UUID_NAME) {
+            fullRoute += "/{$argVal}"
+            //Log.d(LOG_TAG, "Built base route $fullRoute")
+        } else {
+            fullRoute += "/$argVal"
+            //Log.d(LOG_TAG, "Built specific route $fullRoute")
+        }
+        return fullRoute
+    }
+
+    override val route = buildFullRoute(ARG_UUID_NAME)
+    override val title = R.string.app_name
+
+    override fun buildRoute(vararg args: String?): String = buildFullRoute(args[0] ?: "0")
+
+    @Composable
+    override fun Content(
+        modifier: Modifier,
+        backpackingBuddyViewModel: BackpackingBuddyViewModel,
+        navController: NavHostController,
+        navBackStackEntry: NavBackStackEntry,
+        context: Context
+    ) {
+        val uuidString = navBackStackEntry.arguments?.getString(ARG_UUID_NAME)
+        val tripId = uuidString?.let { UUID.fromString(it) }
+
+        LaunchedEffect(tripId) {
+            tripId?.let { backpackingBuddyViewModel.loadTripByUUID(it) }
+        }
+
+        val trip by backpackingBuddyViewModel.currentTripState.collectAsState()
+
+        trip?.let {
+            ExisitngTripItinerary(
+                trip = it
+            )
+        }
+    }
+
+}
--- app/src/main/java/pangolin/backpackingbuddy/viewmodel/BackpackingBuddyViewModel.kt
+++ app/src/main/java/pangolin/backpackingbuddy/viewmodel/BackpackingBuddyViewModel.kt
@@ -1,6 +1,48 @@
 package pangolin.backpackingbuddy.viewmodel
 
+import android.util.Log
+import androidx.compose.runtime.toMutableStateList
+import androidx.core.content.PackageManagerCompat.LOG_TAG
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import pangolin.backpackingbuddy.data.Trip
+import java.util.UUID
 
 class BackpackingBuddyViewModel(): ViewModel() {
+    companion object {
+        private const val LOG_TAG = "448.BackpackingBuddyViewModel"
+    }
+    private val mTrips = trips.toMutableStateList()
+
+    /**
+     * holds list of all characters stored within the view model
+     */
+    val trips: List<Trip>
+        get() = mTrips.toList()
+
+    private val mCurrentTripState: MutableStateFlow<Trip?> = MutableStateFlow(null)
+
+    val currentTripState: StateFlow<Trip?>
+        get() = mCurrentTripState.asStateFlow()
+
+    /**
+     * Loads a character by id into currentCharacterState, if it exists.  If id is not found
+     * in list of characters, then sets currentCharacterState to null.
+     * @param uuid id to use for character lookup
+     */
+    fun loadTripByUUID(uuid: UUID) {
+        Log.d(LOG_TAG, "loadTripByUUID($uuid)")
+        mCurrentTripState.value = null
+        mTrips.forEach { trip ->
+            if(trip.id == uuid) {
+                Log.d(LOG_TAG, "Trip found! $trip")
+                mCurrentTripState.value = trip
+                return
+            }
+        }
+        Log.d(LOG_TAG, "Trip not found")
+        return
+    }
 }
