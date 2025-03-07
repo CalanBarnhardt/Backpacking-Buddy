86e6623e2d1f8b3ba31feae7d656f8cdc6057163
Lindsey <lindseyshanahan@mines.edu>
2025-03-07 11:00:01 -0700

    added in Bottom Bar

---
+++ app/src/main/java/pangolin/backpackingbuddy/ui/navigation/BottomAppBar.kt
@@ -0,0 +1,20 @@
+package pangolin.backpackingbuddy.ui.navigation
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import pangolin.backpackingbuddy.ui.navigation.specs.IScreenSpec
+import pangolin.backpackingbuddy.viewmodel.BackpackingBuddyViewModel
+
+@Composable
+fun BackpackingBuddyBottomBar(navController: NavHostController, backpackingBuddyViewModel: BackpackingBuddyViewModel, context: Context) {
+    val navBackStackEntryState = navController.currentBackStackEntryAsState()
+    IScreenSpec.BottomBar(
+        backpackingBuddyViewModel = backpackingBuddyViewModel,
+        navController = navController,
+        backStackEntry = navBackStackEntryState.value,
+        context = context
+    )
+}
--- app/src/main/java/pangolin/backpackingbuddy/ui/navigation/specs/IScreenSpec.kt
+++ app/src/main/java/pangolin/backpackingbuddy/ui/navigation/specs/IScreenSpec.kt
@@ -2,12 +2,18 @@ package pangolin.backpackingbuddy.ui.navigation.specs
 
 import android.content.Context
 import android.util.Log
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import pangolin.backpackingbuddy.viewmodel.BackpackingBuddyViewModel
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 
 sealed interface IScreenSpec {
     companion object {
@@ -19,6 +25,14 @@ sealed interface IScreenSpec {
         }
         const val ROOT = "backpackingbuddy"
         val startDestination = "" // TODO
+
+        @Composable
+        @JvmStatic
+        fun BottomBar(backpackingBuddyViewModel: BackpackingBuddyViewModel, navController: NavHostController,
+                   backStackEntry: NavBackStackEntry?, context: Context) {
+            val route = backStackEntry?.destination?.route ?: ""
+            allScreens[route]?.BottomAppBarContent(backpackingBuddyViewModel, navController, backStackEntry, context)
+        }
     }
 
     val route: String
@@ -34,4 +48,58 @@ sealed interface IScreenSpec {
         navBackStackEntry: NavBackStackEntry,
         context: Context
     )
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun BottomAppBarContent(backpackingBuddyViewModel: BackpackingBuddyViewModel, navController: NavHostController,
+                                 backStackEntry: NavBackStackEntry?, context: Context) {
+        BottomAppBar(
+            floatingActionButton = {
+                IconButton(onClick = { navController.navigateUp() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "description"
+                    )
+                }
+            },
+            actions = {
+                BottomAppBarActions(
+                    backpackingBuddyViewModel = backpackingBuddyViewModel,
+                    navController = navController,
+                    backStackEntry = backStackEntry,
+                    context = context
+                )
+            }
+        )
+
+//        TopAppBar(
+//            navigationIcon = if (navController.previousBackStackEntry != null) {
+//                {
+//                    IconButton(onClick = { navController.navigateUp() }) {
+//                        Icon(
+//                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+//                            contentDescription = "description"
+//                        )
+//                    }
+//                }
+//            } else {
+//                { }
+//            },
+//            title = {
+//                Text(text = stringResource(id = title))
+//            },
+//            actions = {
+//                BottomAppBarActions(
+//                    backpackingBuddyViewModel = backpackingBuddyViewModel,
+//                    navController = navController,
+//                    backStackEntry = backStackEntry,
+//                    context = context
+//                )
+//            }
+//        )
+    }
+
+    @Composable
+    fun BottomAppBarActions(backpackingBuddyViewModel: BackpackingBuddyViewModel, navController: NavHostController,
+                         backStackEntry: NavBackStackEntry?, context: Context)
 }
