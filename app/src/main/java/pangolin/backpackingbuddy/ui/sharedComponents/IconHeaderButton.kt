aeccb1ec8012d8eafd4583b8550990240c400d76
CalanBarnhardt <crbarnhardt@mines.edu>
2025-03-10 13:46:45 -0600

    Made login and signup things text fields

--- app/src/main/java/pangolin/backpackingbuddy/ui/sharedComponents/IconHeaderButton.kt
+++ app/src/main/java/pangolin/backpackingbuddy/ui/sharedComponents/IconHeaderButton.kt
@@ -9,7 +9,12 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -19,18 +24,21 @@ import pangolin.backpackingbuddy.R
 
 @Composable
 fun IconHeaderButton(headerText: Int, buttonText: Int, icon: ImageVector) {
+    var text by remember { mutableStateOf("") }
+
     Column {
         Text(text = stringResource(headerText))
-        Button(onClick = {}) {
-            Row {
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(stringResource(buttonText)) },
+            leadingIcon = {
                 Icon(
                     imageVector = icon,
                     contentDescription = "Icon",
                     modifier = Modifier.size(24.dp)
                 )
-                Spacer(modifier = Modifier.size(8.dp))
-                Text(stringResource(buttonText))
             }
-        }
+        )
     }
 }
